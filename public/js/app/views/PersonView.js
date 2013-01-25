b75a0f6fb93265e4174b7f0b8f7552923b9bc8c9
Carlos Mecha <mecha.carlos@gmail.com>
2013-01-25 19:40:34 +0100

    Add cache elements.

--- public/js/app/views/PersonView.js
+++ public/js/app/views/PersonView.js
@@ -3,8 +3,6 @@ define(['jquery', 'backbone', 'models/Person', 'text!templ/person.html'],
         var PersonView = Backbone.View.extend({
             el: '#content',
             containsErrors: false,
-            $errorsCache: {},
-            $inputsCache: {},
             rendered: false,
             initialize: function (person) {
                 this.person = person;
@@ -14,17 +12,30 @@ define(['jquery', 'backbone', 'models/Person', 'text!templ/person.html'],
                 'submit form': 'submit'
             },
             render: function () {
-                var self = this;
                 this.$el.html(_.template(personTemplate, this.person.toJSON()));
+                this.rendered = true;
+                this.fillCache();
+                if (this.containsErrors) this.clearErrors();
+                return this;
+            },
+            fillCache: function () {
+                if (!this.rendered) return;
+                this.$cache = {};
+                this.$cache = {}
+                this.$cache.error = {};
+                this.$cache.error.fields = {};
+                this.$cache.inputs = {};
+                var self = this;
+                this.$el.find('.errors').each(function (i) {
+                    self.$cache.error.container = $(this).hide();    
+                });
                 this.$el.find('span[data-error-for]').each(function (i) {
-                    self.$errorsCache[$(this).data('errorFor')] = $(this);
+                    self.$cache.error.fields[$(this).data('errorFor')] = $(this).hide();
                 });
                 this.$el.find('input[data-input]').each(function (i) {
-                    self.$inputsCache[$(this).data('input')] = $(this);
+                    self.$cache.inputs[$(this).data('input')] = $(this);
                 });
-                if (this.containsErrors) this.clearErrors();
-                this.rendered = true;
-                return this;
+                return $cache;
             },
             submit: function () {
                 var $form = this.$el.find('form');
@@ -37,12 +48,14 @@ define(['jquery', 'backbone', 'models/Person', 'text!templ/person.html'],
             printErrors: function (model, errors, options) {
                 var self = this;
                 if (!this.rendered) return;
+                if (this.$cache.error.container) this.$cache.error.container.show();
                 if (_.isObject(errors)) { 
                     _.map(errors, function (error, field) {
-                        if (self.$errorsCache[field]) {
-                            self.$errorsCache[field].text(error);
-                            if (self.$inputsCache[field]) {
-                                self.$inputsCache[field].addClass('input-error');
+                        if (self.$cache.error.fields[field]) {
+                            self.$cache.error.fields[field].show().text(error);
+
+                            if (self.$cache.inputs[field]) {
+                                self.$cache.inputs[field].addClass('input-error');
                             }
                         }
                     });
@@ -50,12 +63,13 @@ define(['jquery', 'backbone', 'models/Person', 'text!templ/person.html'],
                 }
             },
             clearErrors: function () {
-                if (!this.containsErrors) return;
+                if (!this.rendered && !this.containsErrors) return;
+                if (this.$cache.error.container) this.$cache.error.container.hide();
                 for (var error in this.$errorsCache) {
-                    this.$errorsCache[error].text('');
+                    this.$cache.error.fields[error].hide().text('');
                 }
                 for (var input in this.$inputsCache) {
-                    this.$inputsCache[input].removeClass('input-error');
+                    this.$cache.inputs[input].removeClass('input-error');
                 }
                 this.containsErrors = false;
             }
